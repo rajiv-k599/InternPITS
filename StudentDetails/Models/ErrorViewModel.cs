980f512d9ef64230af1b9d216fe997549cd80edc
Rajiv <rajiv.kamat599@gmail.com>
2022-08-24 12:23:26 +0545

    first commit

---
+++ StudentDetails/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentDetails.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
